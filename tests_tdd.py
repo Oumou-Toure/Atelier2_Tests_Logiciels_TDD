56965de313d85922a4de052c6ce4802bf2899bc1
Oumou-Toure <toureoumi203@gmail.com>
2026-01-18 04:59:14 +0100

    RED - test initialisation de la quantité d'une substance connue à zéro

---
+++ tests_tdd.py
@@ -0,0 +1,12 @@
+import unittest
+from laboratory import Laboratory
+
+class TestLaboratory(unittest.TestCase):
+    
+    def test_quantite_substance_connue_initialisee_a_zero(self):
+        substances_laboratoire = Laboratory(["farine", "sucre"])
+        self.assertEqual(substances_laboratoire.getQuantity("farine"), 0.0)
+        self.assertEqual(substances_laboratoire.getQuantity("sucre"), 0.0)
+        
+if __name__ == '__main__':
+    unittest.main()
