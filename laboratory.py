0525813d53c9ec718fa3d83aaa1d01654d3560cc
Oumou-Toure <toureoumi203@gmail.com>
2026-01-18 06:20:07 +0100

    ETAPE 1 GREEN - implémentation de getQuantity pour gérer les pluriels

--- laboratory.py
+++ laboratory.py
@@ -5,6 +5,9 @@ class Laboratory:
     def getQuantity(self, substance): 
         if not isinstance(substance, str):
             raise TypeError("Le nom de la substance doit être une chaîne de caractères.")
+        
+        substance = substance.rstrip("s")
+        
         if substance not in self.stock:
             raise ValueError(f"La substance demandée est inconnue: {substance}")
         return self.stock[substance]
