15e01b951343ced58a61cc9da5424318b83f5ed8
Oumou-Toure <toureoumi203@gmail.com>
2026-01-18 05:30:23 +0100

    ETAPE 1 RED - getQuantity pour une substance inconnue

--- tests_tdd.py
+++ tests_tdd.py
@@ -8,5 +8,12 @@ class TestLaboratory(unittest.TestCase):
         self.assertEqual(substances_laboratoire.getQuantity("farine"), 0.0)
         self.assertEqual(substances_laboratoire.getQuantity("sucre"), 0.0)
         
+    def test_quantite_substance_inconnue(self):
+        
+        substances_laboratoire = Laboratory(["farine", "sucre", "levure"])
+        with self.assertRaises(ValueError):
+            substances_laboratoire.getQuantity("lait")
+
+        
 if __name__ == '__main__':
     unittest.main()
