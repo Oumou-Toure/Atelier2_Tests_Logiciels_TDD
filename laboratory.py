cfacedab4005c9ea29aaf8b069f9491596f4586e
Oumou-Toure <toureoumi203@gmail.com>
2026-01-18 05:37:06 +0100

    ETAPE 1 GREEN - implémentation de la fonction getQuantity pour gérer les substances inconnues

--- laboratory.py
+++ laboratory.py
@@ -3,4 +3,6 @@ class Laboratory:
         self.substances = substances 
         self.stock = {name: 0.0 for name in substances} 
     def getQuantity(self, substance): 
+        if substance not in self.stock:
+            raise ValueError(f"La substance demandée est inconnue: {substance}")
         return self.stock[substance]
